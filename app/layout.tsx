e267ccd2a4d6cbc54caa633fa4dc8c07d1adaf47
Pratham <162104630+Pratz2005@users.noreply.github.com>
2024-12-13 18:11:27 +0800

    kk

---
+++ app/layout.tsx
@@ -0,0 +1,53 @@
+import { inter } from '@/app/ui/fonts';
+import '@/app/ui/global.css';
+
+export default function RootLayout({children} : {children: React.ReactNode}){
+  return(
+    <html lang='eng'>
+      <body className={`${inter.className} antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
